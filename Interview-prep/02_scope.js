e729de1277c581e9f37e7f6a21f83579f2bffd47
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-11-08 07:56:12 +0530

    Update 02_scope.js

--- Interview-prep/02_scope.js
+++ Interview-prep/02_scope.js
@@ -16,7 +16,7 @@ console.log(name); // Accessible here too
 // 2. Function Scope (Local Scope) :-- Variables declared inside a function are accessible only within that function.
 function test() {
   let message = "Inside function";
-  console.log(message); // Accessible here
+  console.log(message); // Accessible 
 }
 
 test();
