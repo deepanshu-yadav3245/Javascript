845ef1fe116c1b916839ddaf0bc3f6740f067c25
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-07 17:39:00 +0530

    update

---
+++ Basic of js/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all JS code as newer version
+
+//  alert (3 + 3) // we are using nodejs, not browser
+
+
+let name = "rahul"
+let age = 18;
+let isLogginIn = false;
+
+//  Types of datatype in JS
+
+// number => 2 to power 53
+// bigint
+// string ("".'')
+// boolean (true , false)
+// null = standlone value
+// undefined 
+// symbol = used for unique
+
+// Object 
+
+console.log(typeof null)  // object 
+console.log(typeof undefined) // undefined 
