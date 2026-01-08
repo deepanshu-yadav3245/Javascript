39826bb83460f658234e3b8ee82f3227090b5cbd
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-08 09:51:10 +0530

    Update type-conversion.js

--- Basic of js/type-conversion.js
+++ Basic of js/type-conversion.js
@@ -30,3 +30,12 @@ let value = 3
 let negValue = -value
 console.log(negValue);
 
+let str1 = "Hello";
+let str2 = "Rahul";
+
+let str3 = str1  + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 3 + 3);
