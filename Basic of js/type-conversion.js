73234defc18a764a5d09bf26260013a4b6fb421f
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-08 09:39:34 +0530

    update

--- Basic of js/type-conversion.js
+++ Basic of js/type-conversion.js
@@ -2,9 +2,24 @@
 
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLogginIn = 1
+
+let booleanIsLoggedIn = Boolean(isLogginIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true;  0 => false
+// "" => false
+// "rahul" => true
+
+ let someNumber = 33
+
+ let stringNumber = String(someNumber)
+ console.log(stringNumber);
+ console.log(typeof stringNumber)
