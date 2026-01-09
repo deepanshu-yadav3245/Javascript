b6fc09f37d79df5d989d9c9f829b13dd04ccd919
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-09 19:33:01 +0530

    Update 05_nums_and_math.js

--- Basic of js/05_nums_and_math.js
+++ Basic of js/05_nums_and_math.js
@@ -35,3 +35,12 @@ let randomNum = Math.random();   // Random number between 0 and 1
 // *********************************Maths*********************************
 
 console.log(Math);
+
+// console.log(Math.PI); // 3.141592653589793
+// console.log(Math.E);  // 2.718281828459045  
+// console.log(Math.SQRT2); // 1.4142135623730951
+// console.log(Math.SQRT1_2);
+// console.log(Math.LN2); // 0.6931471805599453
+// console.log(Math.LN10); // 2.302585092994046
+// console.log(Math.LOG2E); // 1.4426950408889634
+// console.log(Math.LOG10E); // 0.4342944819032518
