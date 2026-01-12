fc9493eb1f4b16327d1bdce5b108278df50210ec
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-12 20:04:18 +0530

    Create 01_function.js

---
+++ 03_basics/01_function.js
@@ -0,0 +1,8 @@
+// What is a Function?
+// A function is a reusable block of code that performs a specific task.
+// Functions help to organize code, make it more readable, and allow for code reuse.    
+
+// Defining a Function
+function greet(name) {
+    console.log("Hello, " + name + "!");
+}
