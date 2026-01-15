7641aa4a9fb3d771860dd93969c17651fa9dc2ef
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-15 22:13:25 +0530

    Update one.js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,7 @@
+// If statement :-- is used to execute a block of code only if a specified condition is true.
+
+// Syntax :--
+// if (condition) {
+//     // code to be executed if condition is true
+// }    
+
