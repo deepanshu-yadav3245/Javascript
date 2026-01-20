7fedb27f535cec1c9acebefee924e797d276248c
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-20 23:27:37 +0530

    Update filter map.js

--- 05_iterations/filter map.js
+++ 05_iterations/filter map.js
@@ -24,3 +24,9 @@ console.log("=====================================================")
 // chaining ekk ya do method ekk sath laga sakte hai
 
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const newNums = myNums
+                .map( (num) => num * 10)
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 40)
+console.log(newNums)
