67a0811c93ecd4e8b5760e894fe3e2ba3c53df69
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-20 22:49:38 +0530

    Update filter map.js

--- 05_iterations/filter map.js
+++ 05_iterations/filter map.js
@@ -1,6 +1,6 @@
-const coding = ['js', 'ruby', 'java', 'python', 'cpp']
+// const coding = ['js', 'ruby', 'java', 'python', 'cpp']
 
-const values = coding.forEach( (item) => {
-    console.log(item);
-})
-console.log(values)
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+// })
+// console.log(values)
