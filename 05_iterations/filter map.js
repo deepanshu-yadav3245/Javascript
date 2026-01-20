4d44b8f797687a6f033f381b48052afd22692abe
deepanshu yadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2026-01-20 23:18:45 +0530

    Update filter map.js

--- 05_iterations/filter map.js
+++ 05_iterations/filter map.js
@@ -11,3 +11,10 @@ const newNums = myNums.filter( (num) => {
     return num > 4
 })
 console.log(newNums);
+
+console.log('=====================================================')
+
+const favNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+const newFav = favNumbers.map( (num) => num + 10)
+console.log(newFav)
